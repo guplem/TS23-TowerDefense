66a9cab7341beea1f9d809814c792766453f183e
guplem <guplem@gmail.com>
2022-05-14 15:14:12 +0200

    Phases done

--- TS23-IggyPoy-Unity/Assets/_IggyPoy/Managers/AppManager.cs
+++ TS23-IggyPoy-Unity/Assets/_IggyPoy/Managers/AppManager.cs
@@ -1,22 +1,15 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AppManager : MonoBehaviour
 {
-    private static AppManager instance;
+    public static AppManager instance;
     
     private void Awake()
     {
         if (instance == null)
-        {
             instance = this;
-        }
         else
-        {
             Destroy(this.gameObject);
-        }
     }
 
 }
--- TS23-IggyPoy-Unity/Assets/_IggyPoy/Managers/GameManager.cs
+++ TS23-IggyPoy-Unity/Assets/_IggyPoy/Managers/GameManager.cs
@@ -1,18 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;
-    
+    public static GameManager instance;
+    public GameData gameData = new GameData(); //{ get; private set; }
+
+    public GamePhase currentGamePhase {
+        get => _currentGamePhase;
+        set {
+            if (value != _currentGamePhase)
+            {
+                _currentGamePhase = value;
+                UIManager.instance.Refresh();
+            }
+        }
+    }
+
+    public GamePhase _currentGamePhase = GamePhase.Construction;
+
     private void Awake()
     {
         if (instance != null)
         {
             Debug.LogWarning("Two GameManagers are existing at the same time. Deleting the old one", this);
+            Destroy(instance.gameObject);
         }
-        Destroy(instance.gameObject);
+
         instance = this;
     }
+
+    private void Start()
+    {
+        UIManager.instance.Refresh();
+    }
+
+    public void SwitchPhase()
+    {
+        if (currentGamePhase == GamePhase.Construction)
+            StartDefensePhase();
+        else
+            StartConstructionPhase();
+    }
+
+    public void StartDefensePhase()
+    {
+        currentGamePhase = GamePhase.Defense;
+    }
+
+    public void StartConstructionPhase()
+    {
+        currentGamePhase = GamePhase.Construction;
+    }
+
+    public enum GamePhase
+    {
+        Construction,
+        Defense,
+    }
+    
 }
