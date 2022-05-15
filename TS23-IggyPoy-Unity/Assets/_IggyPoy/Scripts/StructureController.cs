ab6aec2b9c06f2aa13057100301f7c9495c1aa63
guplem <guplem@gmail.com>
2022-05-15 11:52:56 +0200

    Structures Stats

--- TS23-IggyPoy-Unity/Assets/_IggyPoy/Scripts/StructureController.cs
+++ TS23-IggyPoy-Unity/Assets/_IggyPoy/Scripts/StructureController.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
 
 public class StructureController : HealthController
 {
     [SerializeField] private GameObject visuals;
     [SerializeField] private GameObject blueprint;
+    [Tooltip("Effect (attack, healing, ...) range of the structure")]
+    [SerializeField] private float range = 10;
+    [Tooltip("Minimum allowed distance to the closes building of the same type")]
+    [SerializeField] private float exclusionArea = 2;
+    [Tooltip("The cost to build the structure")]
+    [SerializeField] private float cost = 20;
 
     public void ShowBlueprint()
     {
@@ -15,5 +22,13 @@ public class StructureController : HealthController
     {
         visuals.SetActive(true);
         blueprint.SetActive(false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, exclusionArea);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
