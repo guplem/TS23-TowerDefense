5a542cfe8d0b23a7f56dda96bf832d91dd2d8f40
guplem <11029629+guplem@users.noreply.github.com>
2022-06-20 00:54:00 +0200

    UI allows the construction of all buildings

--- TS23-IggyPoy-Unity/Assets/_IggyPoy/Scripts/GameData.cs
+++ TS23-IggyPoy-Unity/Assets/_IggyPoy/Scripts/GameData.cs
@@ -6,5 +6,5 @@ public class GameData
         get => _resources;
         set { _resources = value; UIManager.instance.FullRefresh(); } // TODO: Change for a refresh only of the needed section
     }
-    private int _resources = 100;
+    private int _resources = 750;
 }
