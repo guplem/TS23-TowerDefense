02009065bd16dc55b32477b89565e5bb27a9dea8
guplem <11029629+guplem@users.noreply.github.com>
2022-06-20 19:18:30 +0200

    Healer unit working

---
+++ TS23-IggyPoy-Unity/Assets/_IggyPoy/Scripts/HealUnitsAround.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealUnitsAround : MonoBehaviour
+{
+
+    [SerializeField] private float delayBetweenHeals = 2;
+    [SerializeField] private int healingPerSecondPerUnit = 10;
+    [SerializeField] private float healingRadius = 10;
+    
+    void Start()
+    {
+        StartCoroutine(HealingCoroutine());
+    }
+
+    private IEnumerator HealingCoroutine()
+    {
+        int maxColliders = 100;
+        Collider[] hitColliders = new Collider[maxColliders];
+        while (true)
+        {
+            HashSet<UnitController> unitsToHeal = new();
+            int numColliders = Physics.OverlapSphereNonAlloc(this.transform.position, healingRadius, hitColliders);
+            for (int i = 0; i < numColliders; i++)
+            {
+                Collider hitCollider = hitColliders[i];
+                if (hitCollider.isTrigger)
+                    continue;
+                UnitController controller = hitCollider.GetComponent<UnitController>();
+                if (controller != null)
+                    if (controller.maxHealth > controller.health)
+                        unitsToHeal.Add(controller);
+            }
+
+            if (unitsToHeal.Count > 0)
+            {
+                unitsToHeal.DebugLog(", ", "Healing " + unitsToHeal.Count + " units: ");
+
+                foreach (UnitController unitController in unitsToHeal)
+                {
+                    unitController.health += Mathf.RoundToInt(healingPerSecondPerUnit * delayBetweenHeals);
+                }
+            }
+            
+            yield return new WaitForSeconds(delayBetweenHeals);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, healingRadius);
+    }
+}
