0a7e02ec57eb74a7bf017cd30b7511a17b60062c
guplem <11029629+guplem@users.noreply.github.com>
2022-06-29 21:49:16 +0200

    Kill stucked enemies

---
+++ TS23-IggyPoy-Unity/Assets/_IggyPoy/Scripts/DestroyAfterTimeIfNotCloseToCenter.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTimeIfNotCloseToCenter : MonoBehaviour
+{
+    [SerializeField] private float time = 30;
+    [SerializeField] private float minDistToCenterToKillRelativeToRadius = 0.6f;
+    private float distToKill => GameManager.instance.mapManager.mapConfiguration.mapRadius * minDistToCenterToKillRelativeToRadius;
+    
+    void Start()
+    {
+        Invoke(nameof(CustomDestroy), time);
+    }
+
+    void CustomDestroy()
+    {
+        if (Vector3.Distance(transform.position, Vector3.zero) > distToKill)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(Vector3.zero, distToKill);
+    }
+}
