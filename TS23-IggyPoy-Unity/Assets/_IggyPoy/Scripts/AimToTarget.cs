3f76a79be6aad2037765f5eb6fd1256e1cbba02f
guplem <11029629+guplem@users.noreply.github.com>
2022-07-01 14:59:02 +0200

    Sniper rotation

---
+++ TS23-IggyPoy-Unity/Assets/_IggyPoy/Scripts/AimToTarget.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimToTarget : MonoBehaviour
+{
+    [SerializeField] private AttackController attController;
+    [Space]
+    [SerializeField] private float speed = 4;
+    [Space]
+    [SerializeField] private bool lock_x = false;
+    [SerializeField] private bool lock_y = false;
+    [SerializeField] private bool lock_z = false;
+
+    void Update()
+    {
+        if (attController == null || attController.target == null)
+            return;
+        
+        Quaternion targetRotation = Quaternion.LookRotation(attController.target.transform.position - transform.position);
+        //transform.LookAt(attController.target.transform);
+        
+        Quaternion rotation = transform.rotation;
+
+        // if (lock_x)
+        //     rotation.x = 0;
+        // if (lock_y)
+        //     rotation.y = 0;
+        // if (lock_z)
+        //     rotation.z = 0;
+
+        //transform.rotation = rotation;
+
+        transform.rotation = Quaternion.Euler(new Vector3(lock_x ? 0f : rotation.eulerAngles.x, lock_y ? 0f : rotation.eulerAngles.y, lock_z ? 0f : rotation.eulerAngles.z));
+        
+        // transform.rotation = Quaternion.Euler(new Vector3(lock_x ? 0f : transform.rotation.eulerAngles.x, lock_y ? 0f : transform.rotation.eulerAngles.y, lock_z ? 0f : transform.rotation.eulerAngles.z) );
+    }
+}
